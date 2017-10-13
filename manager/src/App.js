d5abfd67f1f8dac5d5b103bd81f38bec8f7f4904
An, Albert <contendera@gmail.com>
2017-10-13 12:12:10 -0700

    conected firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -16,7 +16,25 @@ import {
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
+
 class App extends Component {
+
+
+  componentWillMount() {
+    
+    const config = {
+              apiKey: "AIzaSyD5LprzQTVdAj0P7FYCx2ek0xuMJC7x7IQ",
+              authDomain: "project-2382567055602623632.firebaseapp.com",
+              databaseURL: "https://project-2382567055602623632.firebaseio.com",
+              projectId: "project-2382567055602623632",
+              storageBucket: "project-2382567055602623632.appspot.com",
+              messagingSenderId: "265869443051"
+            };
+            firebase.initializeApp(config);
+        }//mount
+
+
   render(){
     return (
       <Provider store={createStore(reducers)}>
